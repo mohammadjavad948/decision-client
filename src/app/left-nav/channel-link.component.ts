7aac46754f9ecca9f58c18fcda22a9cd4c3d706f
mohammad javad <mjkmo123@gmail.com>
2020-12-24 15:40:11 +0330

    add some margin

--- src/app/left-nav/channel-link.component.ts
+++ src/app/left-nav/channel-link.component.ts
@@ -9,7 +9,7 @@ import {Component, Input, OnInit} from '@angular/core';
     </div>
   `,
   styles: [
-    '.back{width: 100%; height: 30px; display: flex; align-items: center; border-radius: 8px; cursor: pointer;}',
+    '.back{width: 100%; height: 30px; display: flex; align-items: center; border-radius: 8px; cursor: pointer; margin-top: 5px;}',
     'span{color: white; margin-left: 5px; font-size: 16px; font-weight: 500;}',
     '.back:hover{background: rgba(255,255,255,0.1); }',
     '.active{background: rgba(255,255,255,0.4);}',
