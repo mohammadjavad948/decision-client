65a1aa90a5d851b9d2dad9719c56e17288200217
mohammad javad <mjkmo123@gmail.com>
2020-12-24 12:24:40 +0330

    change scroll bar

--- src/app/left-nav/channel-list.component.ts
+++ src/app/left-nav/channel-list.component.ts
@@ -3,11 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-channel-list',
   template: `
-    <p>
-      channel-list works!
-    </p>
+    <div class="list"></div>
   `,
   styles: [
+    '.list{width: 100%; height: -webkit-fill-available; overflow: hidden auto;}',
+    '::-webkit-scrollbar {width: 10px;}',
+    '::-webkit-scrollbar-track {background: #282c3382;}',
+    '::-webkit-scrollbar-track {background: #888;}',
+    '::-webkit-scrollbar-track:hover {background: #555; }',
   ]
 })
 export class ChannelListComponent implements OnInit {
