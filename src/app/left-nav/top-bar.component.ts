4d5b0b2c1b163d6cfe957099f2c1b0427cd9ed48
mohammad javad <mjkmo123@gmail.com>
2020-12-24 11:43:46 +0330

    add cog image

--- src/app/left-nav/top-bar.component.ts
+++ src/app/left-nav/top-bar.component.ts
@@ -5,13 +5,15 @@ import { Component, OnInit } from '@angular/core';
   template: `
     <div class="top-bar">
       <span class="text">{{appName}}</span>
+      <img src="/assets/image/cog.svg" alt="cog" class="cog">
     </div>
     <hr>
   `,
   styles: [
-    '.top-bar{width: 100%; height: 40px; display: flex; align-items: center; justify-content: space-between; color: white;}',
+    '.top-bar{width: 100%; height: 48px; display: flex; align-items: center; justify-content: space-between; color: white;}',
     'hr{margin: unset;}',
-    '.text{margin-left: 5px; font-size: 17px; line-height: 25px; font-weight: 700;}'
+    '.text{margin-left: 5px; font-size: 17px; line-height: 25px; font-weight: 700;}',
+    '.cog{width: 20px; margin-right: 5px;}'
   ]
 })
 export class TopBarComponent implements OnInit {
