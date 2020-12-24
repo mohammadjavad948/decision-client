3dd5c0fe047b682ec14438d342e4aa7718d633df
mohammad javad <mjkmo123@gmail.com>
2020-12-24 11:38:57 +0330

    add title to top bar

--- src/app/left-nav/top-bar.component.ts
+++ src/app/left-nav/top-bar.component.ts
@@ -3,14 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-top-bar',
   template: `
-    <p>
-      top-bar works!
-    </p>
+    <div class="top-bar">
+      <span class="text">{{appName}}</span>
+    </div>
+    <hr>
   `,
   styles: [
+    '.top-bar{width: 100%; height: 40px; display: flex; align-items: center; justify-content: space-between; color: white;}',
+    'hr{margin: unset;}',
+    '.text{margin-left: 5px; font-size: 17px; line-height: 25px; font-weight: 700;}'
   ]
 })
 export class TopBarComponent implements OnInit {
+  appName = 'decision core';
 
   constructor() { }
 
