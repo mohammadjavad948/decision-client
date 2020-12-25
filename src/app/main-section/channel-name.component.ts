5cab148ae5dca5ba9d60e984b036b693f4eb8ef1
mohammad javad <mjkmo123@gmail.com>
2020-12-25 18:03:17 +0330

    add flex to texts

--- src/app/main-section/channel-name.component.ts
+++ src/app/main-section/channel-name.component.ts
@@ -4,13 +4,15 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-channel-name',
   template: `
     <div class="top">
-      <span>channel 1</span>
+      <span class="text">channel 1</span>
     </div>
     <hr>
   `,
   styles: [
-    '.top{display: block; width: 100%; height: 48px; color: white; background: #393e46}',
-    'hr{margin: unset;}'
+    // tslint:disable-next-line:max-line-length
+    '.top{display: flex; width: 100%; height: 48px; color: white; background: #393e46; align-items: center; justify-content: space-evenly;}',
+    'hr{margin: unset;}',
+    '.text{font-size: 17px;}'
   ]
 })
 export class ChannelNameComponent implements OnInit {
