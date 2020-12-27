fa89fd638011d22cb8be85dfb8caadbb32b5acf1
mohammad javad <mjkmo123@gmail.com>
2020-12-27 20:23:15 +0330

    add select a channel

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {MainSectionComponent} from './main-section/main-section.component';
+import {SelectAChannelComponent} from './select-a-channel/select-a-channel.component';
 
 const routes: Routes = [
+  {
+    path: '',
+    component: SelectAChannelComponent
+  },
   {
     path: ':id',
     component: MainSectionComponent
