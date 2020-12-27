ea8e25c0a6546788c81ff09e5d321dd4443279c9
mohammad javad <mjkmo123@gmail.com>
2020-12-27 09:13:35 +0330

    finally we can connect to socket

--- src/app/main-section/main-section.component.ts
+++ src/app/main-section/main-section.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {Socket} from 'ngx-socket-io';
+import { io } from 'socket.io-client';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-main-section',
@@ -11,9 +12,14 @@ export class MainSectionComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    
+    this.connectToSocket();
   }
   connectToSocket(): void{
-    
+    console.log('connnecting to socket');
+    const socket = io(environment.endpoint);
+
+    socket.on('connect', () => {
+        console.log(socket.id);
+    });
   }
 }
